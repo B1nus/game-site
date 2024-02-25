ca2ac55d30c36c05d4325344726fb5d490503cb6
B1nus <119787571+B1nus@users.noreply.github.com>
2024-02-25 20:43:14 +0100

    edited game table, questions pre memoria and a rudimentary search bar.

--- app.rb
+++ app.rb
@@ -3,10 +3,16 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 
+# Återkoppla användaren till browse sidan som fungerar som index sida
 get('/') do
     redirect('/browse')
 end
 
+# Visa sidan där man kan söka efter spel
 get('/browse') do
     slim(:browse)
 end
+
+# Restful routes viktigt? (blir simplare utan dem i mitt fall)
+# Domänbeskrivning var?
+# Yardoc?
