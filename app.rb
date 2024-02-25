d7c1f81f097d0dfafae8af4f50cd2b7b57a271c5
B1nus <119787571+B1nus@users.noreply.github.com>
2024-02-25 20:43:14 +0100

    github cli test

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
+require "./helper_functions"
 
 # Återkoppla användaren till browse sidan som fungerar som index sida
 get('/') do
@@ -10,6 +11,8 @@ end
 
 # Visa sidan där man kan söka efter spel
 get('/browse') do
+    db = connect_to_default_database()
+    
     slim(:browse)
 end
 
