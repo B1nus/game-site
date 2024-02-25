0292aec623b2e4ec115a75d3ea4bd83c7cced10f
B1nus <119787571+B1nus@users.noreply.github.com>
2024-02-25 20:43:14 +0100

    spel funktionalitets test

--- app.rb
+++ app.rb
@@ -6,15 +6,35 @@ require "./helper_functions"
 
 # Återkoppla användaren till browse sidan som fungerar som index sida
 get('/') do
-    redirect('/browse')
+    redirect('/games')
 end
 
 # Visa sidan där man kan söka efter spel
-get('/browse') do
+get('/games') do
     db = connect_to_default_database()
-    
-    slim(:browse)
+
+    games = db.execute("SELECT * FROM game")
+
+    slim(:"games/index", locals:{games:games})
 end
+
+# Spela ett spel
+get('/games/:id') do
+    db = connect_to_default_database()
+
+    game_id = params[:id]
+
+    game = db.execute("SELECT * FROM game WHERE id = ?", game_id).first
+    game_url = game["foreign_url"]
+    foreign_url = game["foreign_url"]
+    resolution = {width:game["resolution_width"], height:game["resolution_width"]}
+
+    puts "game_id: #{game_id} game_url: #{game_url} foreign: #{foreign_url}"
+
+    slim(:"games/show", locals:{game_url:game_url, foreign_url:foreign_url, resolution:resolution})
+end
+
+# 
 
 # Restful routes viktigt? (blir simplare utan dem i mitt fall)
 # Domänbeskrivning var?
