0009eac437fc614cd673cb2263fe4f8a6ec1712d
B1nus <119787571+B1nus@users.noreply.github.com>
2024-03-18 14:27:46 +0100

    formatterade app.rb med rubyfmt

--- app.rb
+++ app.rb
@@ -1,227 +1,229 @@
-require 'sinatra'
-require 'slim'
-require 'sinatra/reloader'
-require "./model"
 require "erb"
-require 'sinatra/flash'
+require "./model"
+require "sinatra"
+require "sinatra/reloader"
+require "sinatra/flash"
+require "slim"
 
 # Länk för att komma in på hemsidan snabbt under testning (:
-p "http://localhost:4567/"
+p("http://localhost:4567/")
 
-enable :sessions
+enable(:sessions)
 
 # Yardoc
-include Model
+include(Model)
 
 # Återkoppla användaren till browse sidan som fungerar som landing page
-get('/') do
-    redirect('/games/')
+get("/") do
+  redirect("/games/")
 end
 
 # Sidan där man utforskar spel
-get('/games/') do
-    @games = database_games() # Alla attribut från alla spel är publika!!!
+get("/games/") do
+  # Alla attribut från alla spel är publika!!!
+  @games = database_games
 
-    @games.each do |game|
-        # Kolla efter en tag som varnar för tredjeparts hemsida
-        if database_game_tag_purposes(game["id"]).include?({"name"=>"warn_for_stolen_game"})
-            game["url"] = "/warning/#{game["id"]}"
-        else
-            game["url"] = "/games/#{game["id"]}"
-        end
+  @games.each do |game|
+    # Kolla efter en tag som varnar för tredjeparts hemsida
+    if database_game_tag_purposes(game["id"]).include?({"name" => "warn_for_stolen_game"})
+      game["url"] = "/warning/#{game["id"]}"
+    else
+      game["url"] = "/games/#{game["id"]}"
     end
+  end
 
-    slim(:"games/index")
+  slim(:"games/index")
 end
 
 # Varning för att jag inte äger spelet
-get('/warning/:game_id') do
-    game = database_game_with_id(params[:game_id])
+get("/warning/:game_id") do
+  game = database_game_with_id(params[:game_id])
 
-    @foreign_url = game["foreign_url"]
-    @indigenous_url = "/games/" + game["id"].to_s
+  @foreign_url = game["foreign_url"]
+  @indigenous_url = "/games/" + game["id"].to_s
 
-    slim(:"warning")
+  slim(:"warning")
 end
 
 # Spela ett spel
-get('/games/:game_id') do
-    game_id = params[:game_id]
+get("/games/:game_id") do
+  game_id = params[:game_id]
 
-    # Alla attribut från alla spel är publika!!!
-    @game = database_game_with_id(game_id)
-    
-    # Ganska dumt att bara ta den första. Men kommer fungera så....
-    game_iframe_size_string = database_game_iframe_size(game_id).first
-    if game_iframe_size_string != nil
-        @iframe_size_css = game_iframe_size_string
-    else
-        # Standard storlek på iframe om inget specifieras
-        @iframe_size_css = "width: 800px; height: 550px;"
-    end
+  # Alla attribut från alla spel är publika!!!
+  @game = database_game_with_id(game_id)
 
-    @allow_fullscreen = database_game_tag_purposes(game_id).include?({"name"=>"game_supports_fullscreen"})
+  # Ganska dumt att bara ta den första. Men kommer fungera så....
+  game_iframe_size_string = database_game_iframe_size(game_id).first
+  if game_iframe_size_string != nil
+    @iframe_size_css = game_iframe_size_string
+  else
+    # Standard storlek på iframe om inget specifieras
+    @iframe_size_css = "width: 800px; height: 550px;"
+  end
 
-    slim(:"games/show")
+  @allow_fullscreen = database_game_tag_purposes(game_id).include?({"name" => "game_supports_fullscreen"})
+
+  slim(:"games/show")
 end
 
 # [ADMIN] Formulär för att lägga till en ny tag.
-get "/tags/new" do 
-    # ONLY AS ADMIN
+get("/tags/new") do
+  # ONLY AS ADMIN
 
-    # För att se vad tag purpose id:n står för
-    @tag_purposes = database_tag_purposes()
+  # För att se vad tag purpose id:n står för
+  @tag_purposes = database_tag_purposes
 
-    erb(:"tags/new")
+  erb(:"tags/new")
 end
 
 # [ADMIN] Add a new tag.
-post "/tags" do
-    # ONLY AS ADMIN
-    tag_name = params[:tag_name]
-    tag_purpose_id = params[:tag_purpose_id]
+post("/tags") do
+  # ONLY AS ADMIN
+  tag_name = params[:tag_name]
+  tag_purpose_id = params[:tag_purpose_id]
 
-    database_create_tag(tag_name, tag_purpose_id)
+  database_create_tag(tag_name, tag_purpose_id)
 
-    redirect "/tags/"
+  redirect("/tags/")
 end
 
 # [ADMIN] Formulär för att ändra en tag.
-get "/tags/:id/edit" do
-    # ONLY AS ADMIN
-    @tag_id = params[:id]
+get("/tags/:id/edit") do
+  # ONLY AS ADMIN
+  @tag_id = params[:id]
 
-    tag = database_tag_with_id(@tag_id)
+  tag = database_tag_with_id(@tag_id)
 
-    # För att placera föregående värden
-    @tag_name = tag["tag_name"]
-    @tag_purpose_id = tag["tag_purpose_id"]
+  # För att placera föregående värden
+  @tag_name = tag["tag_name"]
+  @tag_purpose_id = tag["tag_purpose_id"]
 
-    # För att se vad tag purpose id:n står för
-    @tag_purposes = database_tag_purposes()
+  # För att se vad tag purpose id:n står för
+  @tag_purposes = database_tag_purposes
 
-    erb(:"tags/edit")
+  erb(:"tags/edit")
 end
 
 # [ADMIN] Ändra en tag.
-post "/tags/:id/update" do
-    # ONLY AS ADMIN
-    tag_id = params[:id]
-    tag_name = params[:tag_name]
-    tag_purpose_id = params[:tag_purpose_id]
+post("/tags/:id/update") do
+  # ONLY AS ADMIN
+  tag_id = params[:id]
+  tag_name = params[:tag_name]
+  tag_purpose_id = params[:tag_purpose_id]
 
-    database_edit_tag(tag_id, tag_name, tag_purpose_id)
+  database_edit_tag(tag_id, tag_name, tag_purpose_id)
 
-    redirect "/tags/"
+  redirect("/tags/")
 end
 
 # [ADMIN] Display all tags. (admin på grund utav edit och delete knappen)
-get "/tags/" do
-    @tags = database_tags()
+get("/tags/") do
+  @tags = database_tags
 
-    slim(:"tags/index")
+  slim(:"tags/index")
 end
 
 # Visa en tag. (FÅR INTE VARA FÖRE ROUTEN /tags/new!!!)
-get "/tags/:id" do
-    @tag = database_tag_with_id(params[:id])
-    
-    slim(:"tags/show", locals:{tag:@tag})
+get("/tags/:id") do
+  @tag = database_tag_with_id(params[:id])
+
+  slim(:"tags/show", locals: {tag: @tag})
 end
 
-# [ADMIN] Ta bort en tag. 
-post "/tags/:id/delete" do
-    tag_id = params[:id]
+# [ADMIN] Ta bort en tag.
+post("/tags/:id/delete") do
+  tag_id = params[:id]
 
-    delete_tag(tag_id)
-    
-    redirect "/tags/"
+  delete_tag(tag_id)
+
+  redirect("/tags/")
 end
 
 # Visa listan för tag syften
-get "/tag-purposes/" do
-    @tag_purposes = database_tag_purposes()
+get("/tag-purposes/") do
+  @tag_purposes = database_tag_purposes
 
-    slim(:"tag-purposes/index")
+  slim(:"tag-purposes/index")
 end
 
 # Formulär för att skapa ett nytt tag syfte
-get "/tag-purposes/new" do    
-    slim(:"tag-purposes/new")
+get("/tag-purposes/new") do
+  slim(:"tag-purposes/new")
 end
 
 # [ADMIN] Skapa ett nytt tag Syfte
-post "/tag-purposes" do
-    @tag_purpose_name = params[:tag_purpose_name]
+post("/tag-purposes") do
+  @tag_purpose_name = params[:tag_purpose_name]
 
-    database_create_tag_purpose(@tag_purpose_name)
-    
-    redirect("/tag-purposes/")
+  database_create_tag_purpose(@tag_purpose_name)
+
+  redirect("/tag-purposes/")
 end
 
 # [ADMIN] Redigera ett spel genom formulär m.m. (följer inte restful routes )
-get "/games/:id/edit" do
-    @game_id = params[:id]
+get("/games/:id/edit") do
+  @game_id = params[:id]
 
-    # Kanske borde vis alla tags istället för bara de som är kvar.
-    @available_tags = database_game_available_tags(@game_id)
-    @applied_tags = database_game_applied_tags(@game_id)
+  # Kanske borde vis alla tags istället för bara de som är kvar.
+  @available_tags = database_game_available_tags(@game_id)
+  @applied_tags = database_game_applied_tags(@game_id)
 
-    # Samla alla tags i en lista
-    @tags = database_tags()
-    
-    erb(:"games/edit")
+  # Samla alla tags i en lista
+  @tags = database_tags
+
+  erb(:"games/edit")
 end
 
-post "/games/:id/update" do # Hmmm, jag får bara det sista elementet i selectionen...
-    game_id = params[:id]
-    p params[:tags_selection]
+# Hmmm, jag får bara det sista elementet i selectionen...
+post("/games/:id/update") do
+  game_id = params[:id]
+  p(params[:tags_selection])
 
-    redirect "/games/#{game_id}/edit"
+  redirect("/games/#{game_id}/edit")
 end
 
 # Visa formuläret för att registrera en användare
-get "/register" do
-    erb :"users/register" # OBS. Medveten avvikelse från restuful routes
+get("/register") do
+  # OBS. Medveten avvikelse från restuful routes
+  erb(:"users/register")
 end
 
 # Ska lägga till validering med regex här senare
-post "/register" do
-    username = params[:username]
-    password = params[:password]
+post("/register") do
+  username = params[:username]
+  password = params[:password]
 
-    # Registrerings validering. (Om flash_msg är tom fann vi inga fel på registreringen)
-    flash_msg = ""
-    if username.length == 0
-        flash_msg = "You need to type a username"
-    elsif username == "admin"
-        flash_msg = "Lmao, bro really though he could become an admin"
-    elsif database_does_user_exist?(username)
-        flash_msg = "Username taken, choose another username"
-    elsif password.length == 0
-        flash_msg = "You need to type a password"
-    elsif password.length < 8
-        flash_msg = "Your password needs to be at least 8 characters long"
-    elsif not password =~ /[A-Z]/
-        flash_msg = "Your password needs a capital letter"
-    elsif not password =~ /[0-9]/
-        flash_msg = "Your password needs a number"
-    elsif not password =~ /[#?!@$ %^&*-]/
-        flash_msg = "Your password needs one special character: #?!@$ %^&*-"
-    end
+  # Registrerings validering. (Om flash_msg är tom fann vi inga fel på registreringen)
+  flash_msg = ""
+  if username.length == 0
+    flash_msg = "You need to type a username"
+  elsif username == "admin"
+    flash_msg = "Lmao, bro really though he could become an admin"
+  elsif database_does_user_exist?(username)
+    flash_msg = "Username taken, choose another username"
+  elsif password.length == 0
+    flash_msg = "You need to type a password"
+  elsif password.length < 8
+    flash_msg = "Your password needs to be at least 8 characters long"
+  elsif not password =~ /[A-Z]/
+    flash_msg = "Your password needs a capital letter"
+  elsif not password =~ /[0-9]/
+    flash_msg = "Your password needs a number"
+  elsif not password =~ /[#?!@$ %^&*-]/
+    flash_msg = "Your password needs one special character: #?!@$ %^&*-"
+  end
 
-    if flash_msg == ""
-        # Inga fel hittades
-        "Register route was successfull"
-    else 
-        # fel hittades
-        flash[:notice] = flash_msg
-        redirect('/register')
-    end
+  if flash_msg == ""
+    # Inga fel hittades
+    "Register route was successfull"
+  else
+    # fel hittades
+    flash[:notice] = flash_msg
+    redirect("/register")
+  end
 end
 
-get "/login" do
-    
+get("/login") do
 end
 
 # Restful routes viktigt? Strunta i det för likes, men gör det för tags och spel
