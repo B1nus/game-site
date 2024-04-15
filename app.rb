427b8c42fef05733a1bc50e9ec8d329b1f9002cd
B1nus <119787571+B1nus@users.noreply.github.com>
2024-04-15 07:42:11 +0200

    Försökte igen

--- app.rb
+++ app.rb
@@ -7,202 +7,212 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/flash'
 
-# Used for login
-enable :sessions
+module Web
+  # My website
+  class Main < Sinatra::Base
+    # Used for login
+    enable :sessions
 
-# Yardoc
-# include Model
+    # Yardoc
+    # include Model
 
-# Database
-database = Model.new
+    # Database
+    database = Model.new
 
-on_start { puts 'Server started as http://localhost:4567' }
-on_stop { puts 'By Bye!' }
+    on_start { puts 'Server started as http://localhost:4567' }
+    on_stop { puts 'By Bye!' }
 
-# Needed to expose the database to my './helper.rb' file
-before('*') { @database = database }
+    # Include my get routes
+    include GetRoutes
 
-# Protect admin and user sites
-before('/admin/*') { admin_check }
-before('/user*') { user_check }
+    # Needed to expose the database to my './helper.rb' file
+    before('*') { @database = database }
 
-# Updates an existing game and redirects to '/admin/games'
-post '/admin/games/:id/update' do
-  # name = params[:name]
-  # tag_ids = params[:tags]
-  # fullscreen = !params[:fullscreen].nil?
-  # warning = !params[:warning].nil?
-  # time_created = ???
+    get('/') do 'hello' end
 
-  redirect '/games'
-end
+    # Protect admin and user sites
+    before('/admin/*') { admin_check }
+    before('/user*') { user_check }
 
-# Add a new tag
-#
-# @param tag_name [String]
-# @param tag_purpose_id [Integer]
-post('/admin/tags') do
-  if params[:name].empty?
-    flash[:notice] = 'The tag name can\'t be empty'
-    redirect '/games'
+    # Updates an existing game and redirects to '/admin/games'
+    post '/admin/games/:id/update' do
+      # name = params[:name]
+      # tag_ids = params[:tags]
+      # fullscreen = !params[:fullscreen].nil?
+      # warning = !params[:warning].nil?
+      # time_created = ???
+
+      redirect '/games'
+    end
+
+    # Add a new tag
+    #
+    # @param tag_name [String]
+    # @param tag_purpose_id [Integer]
+    post('/admin/tags') do
+      if params[:name].empty?
+        flash[:notice] = 'The tag name can\'t be empty'
+        redirect '/games'
+      end
+
+      database.add_tag(params[:name], params[:purpose_id].to_i)
+      redirect '/games'
+    end
+
+    # Update a tag
+    #
+    # @param [Integer] id, the id of the tag
+    # @param [String] tag_name, the new name of the tag
+    # @param [Integer] tag_purpose_id, the new tag purpose id
+    #
+    # @see Model#database_edit_tag
+    post('/admin/tags/:id/update') do
+      if params[:name].empty?
+        flash[:notice] = 'The new tag name can\'t be empty'
+        redirect '/games'
+      end
+
+      database.update_tag(params_id, params[:name], params[:purpose_id])
+      redirect '/games'
+    end
+
+    # Remove a tag
+    #
+    # @param [Integer] id, The id for the tag
+    post('/admin/tags/:id/delete') do
+      database.delete_tag(params_id)
+      redirect '/games'
+    end
+
+    # Create a new tag purpose
+    #
+    post('/admin/tag-purposes') do
+      if params[:purpose].empty?
+        flash[:notice] = 'The tag purpose name can\'t be empty'
+        redirect '/games'
+      end
+
+      database.add_tag_purpose(params[:purpose])
+      redirect '/games'
+    end
+
+    # Attempts to register a user
+    #
+    # @param [String] username, The username
+    # @param [String] password, The password
+    # @param [String] repeat-password, The repeated password
+    #
+    # @see Model#register_user
+    post '/register' do
+      cooldown_check '/register'
+
+      if error = database.register_user(params[:username], params[:password], params[:repeat_password])
+        flash[:notice] = error
+        redirect '/register'
+      else
+        flash[:notice] = 'Registration successful!'
+        redirect '/login'
+      end
+    end
+
+    # Attempts to login
+    #
+    # @params [String] username, The username
+    # @params [String] password, The password
+    #
+    # @see Model#login
+    post '/login' do
+      cooldown_check '/login'
+
+      if id = database.login(params[:username], params[:password])
+        # Successful login!
+        change_user_id(id)
+        redirect '/games'
+      else
+        # Login failed.
+        flash[:notice] = 'Invalid login credentials'
+        redirect '/login'
+      end
+    end
+
+    # Logout a user and redirect to homepage
+    #
+    post '/logout' do
+      logout
+
+      redirect '/games'
+    end
+
+    # Attempt to change a users username
+    #
+    # @params [String] username, the username
+    #
+    # @see Model#change_username
+    post '/user/username/update' do
+      cooldown_check '/user'
+
+      flash[:notice] = database.change_username(user_id, params[:username]) || 'Username successfully changed!'
+
+      redirect '/user'
+    end
+
+    # Attempt to change your password
+    #
+    # @params [String] password, your new password
+    # @params [String] repeat_password, same password another time to make sure you remember it
+    #
+    # @see Model#change_password
+    post '/user/password/update' do
+      cooldown_check '/user'
+
+      error = database.change_password(
+        user_id,
+        params[:current_password],
+        params[:password],
+        params[:repeat_password]
+      )
+
+      flash[:notice] = error || 'Password successfully changed!'
+
+      redirect '/user'
+    end
+
+    # Delete the current user
+    #
+    # @see Model#delete_user
+    post '/user/delete' do
+      delete_user user_id
+      logout
+
+      redirect '/games'
+    end
+
+    # Delete a user
+    #
+    # @see Model#delete_user
+    post '/admin/users/:id/delete' do
+      delete_user params_id
+
+      redirect '/admin/users'
+    end
+
+    # Användare crud, Kolla rätt user_id först
+    # Gillning av spel, kolla för inloggad, annars error, kolla även rätt user_id
+    # kanske abtrahera user_id check som med helpers::admin?
+    # /admin/games, edit å delete knapp
+    # flash för lyckad loggin? flash for om du inte är inloggad istället för redirect?
+
+    # Restful routes viktigt? Strunta i det för likes, men gör det för tags och spel
+    # Domän check i app.rb.
+    # Ta bort länken till scratch see inside. Gör så det är till servern istället
+    # Cooldown för inloggningsförsök. request.ip
+    #
+    # Regex validering på några fält. Även med tid för A nivå.
+    #
+    # before /admin/* . Gör så för alla admin routes
+    # gör en separat /admin/games/ index sida med edit och delete knappar
+    # login ip sparning
+    # login validering
+    # login autentisering i model.rb
   end
-
-  database.add_tag(params[:name], params[:purpose_id].to_i)
-  redirect '/games'
 end
-
-# Update a tag
-#
-# @param [Integer] id, the id of the tag
-# @param [String] tag_name, the new name of the tag
-# @param [Integer] tag_purpose_id, the new tag purpose id
-#
-# @see Model#database_edit_tag
-post('/admin/tags/:id/update') do
-  if params[:name].empty?
-    flash[:notice] = 'The new tag name can\'t be empty'
-    redirect '/games'
-  end
-
-  database.update_tag(params_id, params[:name], params[:purpose_id])
-  redirect '/games'
-end
-
-# Remove a tag
-#
-# @param [Integer] id, The id for the tag
-post('/admin/tags/:id/delete') do
-  database.delete_tag(params_id)
-  redirect '/games'
-end
-
-# Create a new tag purpose
-#
-post('/admin/tag-purposes') do
-  if params[:purpose].empty?
-    flash[:notice] = 'The tag purpose name can\'t be empty'
-    redirect '/games'
-  end
-
-  database.add_tag_purpose(params[:purpose])
-  redirect '/games'
-end
-
-# Attempts to register a user
-#
-# @param [String] username, The username
-# @param [String] password, The password
-# @param [String] repeat-password, The repeated password
-#
-# @see Model#register_user
-post '/register' do
-  cooldown_check '/register'
-
-  if error = database.register_user(params[:username], params[:password], params[:repeat_password])
-    flash[:notice] = error
-    redirect '/register'
-  else
-    flash[:notice] = 'Registration successful!'
-    redirect '/login'
-  end
-end
-
-# Attempts to login
-#
-# @params [String] username, The username
-# @params [String] password, The password
-#
-# @see Model#login
-post '/login' do
-  cooldown_check '/login'
-
-  if id = database.login(params[:username], params[:password])
-    # Successful login!
-    change_user_id(id)
-    redirect '/games'
-  else
-    # Login failed.
-    flash[:notice] = 'Invalid login credentials'
-    redirect '/login'
-  end
-end
-
-# Logout a user and redirect to homepage
-#
-post '/logout' do
-  logout
-
-  redirect '/games'
-end
-
-# Attempt to change a users username
-#
-# @params [String] username, the username
-#
-# @see Model#change_username
-post '/user/username/update' do
-  cooldown_check '/user'
-
-  flash[:notice] = database.change_username(user_id, params[:username]) || 'Username successfully changed!'
-
-  redirect '/user'
-end
-
-# Attempt to change your password
-#
-# @params [String] password, your new password
-# @params [String] repeat_password, same password another time to make sure you remember it
-#
-# @see Model#change_password
-post '/user/password/update' do
-  cooldown_check '/user'
-
-  error = database.change_password(
-    user_id,
-    params[:current_password],
-    params[:password],
-    params[:repeat_password]
-  )
-
-  flash[:notice] = error || 'Password successfully changed!'
-
-  redirect '/user'
-end
-
-# Delete the current user
-#
-# @see Model#delete_user
-post '/user/delete' do
-  delete_user user_id
-  logout
-
-  redirect '/games'
-end
-
-# Delete a user
-#
-# @see Model#delete_user
-post '/admin/users/:id/delete' do
-  delete_user params_id
-
-  redirect '/admin/users'
-end
-
-# Användare crud, Kolla rätt user_id först
-# Gillning av spel, kolla för inloggad, annars error, kolla även rätt user_id
-# kanske abtrahera user_id check som med helpers::admin?
-# /admin/games, edit å delete knapp
-# flash för lyckad loggin? flash for om du inte är inloggad istället för redirect?
-
-# Restful routes viktigt? Strunta i det för likes, men gör det för tags och spel
-# Domän check i app.rb.
-# Ta bort länken till scratch see inside. Gör så det är till servern istället
-# Cooldown för inloggningsförsök. request.ip
-#
-# Regex validering på några fält. Även med tid för A nivå.
-#
-# before /admin/* . Gör så för alla admin routes
-# gör en separat /admin/games/ index sida med edit och delete knappar
-# login ip sparning
-# login validering
-# login autentisering i model.rb
--- get_routes.rb
+++ get_routes.rb
@@ -1,80 +1,83 @@
-# frozen_string_literal
-require 'sinatra'
+# frozen_string_literal: true
 
-# guest routes
-get('/') { redirect '/games' }
-get('/home') { redirect '/games' }
+# Get routes for my website
+module GetRoutes
+  def self.included(base)
+    # guest routes
+    base.get('/') { redirect '/games' }
+    base.get('/home') { redirect '/games' }
 
-get('/games') do
-  erb(:'games/index', locals: {
-        games: database.all_of('game'),
-        tags: database.all_of('tag'),
-        tag_purposes: database.all_of('tag_purpose')
-      })
-end
+    base.get('/games') do
+      erb(:'games/index', locals: {
+            games: database.all_of('game'),
+            tags: database.all_of('tag'),
+            tag_purposes: database.all_of('tag_purpose')
+          })
+    end
 
-get('/games/:id') do
-  erb(:'games/show', locals: {
-        game: database.select('game', 'id', params_id),
-        iframe_size: database.game_iframe_sizes(params_id).first
-      })
-end
+    base.get('/games/:id') do
+      erb(:'games/show', locals: {
+            game: database.select('game', 'id', params_id),
+            iframe_size: database.game_iframe_sizes(params_id).first
+          })
+    end
 
-get('/register') do
-  erb :'users/register'
-end
+    base.get('/register') do
+      erb :'users/register'
+    end
 
-get('/login') do
-  erb :'users/login'
-end
+    base.get('/login') do
+      erb :'users/login'
+    end
 
-# user routes
-get('/user') do
-  erb(:'users/edit', locals: {
-        user: database.select('user', 'id', user_id)
-      })
-end
+    # user routes
+    base.get('/user') do
+      erb(:'users/edit', locals: {
+            user: database.select('user', 'id', user_id)
+          })
+    end
 
+    # admin routes
+    base.get('/admin/games/new') do
+      erb :'games/new'
+    end
 
-# admin routes
-get('/admin/games/new') do
-  erb :'games/new'
-end
+    base.get('/admin/games/:id/edit') do
+      erb(:'games/edit', locals: {
+            game: database.select('game', 'id', params_id),
+            game_tags: database.game_tags(params_id),
+            tags: database.all_of('tag')
+          })
+    end
 
-get('/admin/games/:id/edit') do
-  erb(:'games/edit', locals: {
-        game: database.select('game', 'id', params_id),
-        game_tags: database.game_tags(params_id),
-        tags: database.all_of('tag')
-      })
-end
+    base.get('/admin/tags/new') do
+      erb(:'tags/new', locals: { purposes: database.all_of('tag_purpose') })
+    end
 
-get('/admin/tags/new') do
-  erb(:'tags/new', locals: { purposes: database.all_of('tag_purpose') })
-end
+    base.get('/admin/tags/:id/edit') do
+      erb(:'tags/edit', locals: {
+            tag: database.select('tag', 'id', params_id),
+            tag_purposes: database.all_of('tag_purpose')
+          })
+    end
 
-get('/admin/tags/:id/edit') do
-  erb(:'tags/edit', locals: {
-        tag: database.select('tag', 'id', params_id),
-        tag_purposes: database.all_of('tag_purpose')
-      })
-end
+    base.get('/admin/tags/:id') do
+      erb(:'tags/show', locals: {
+            tag: database.select('tag', 'id', params_id)
+          })
+    end
 
-get('/admin/tags/:id') do
-  erb(:'tags/show', locals: {
-        tag: database.select('tag', 'id', params_id)
-      })
-end
+    base.get('/admin/tags') do
+      erb(:'tags/index', locals: {
+            tags: database.all_of('tag'),
+            tag_purposes: database.all_of('tag_purpose')
+          })
+    end
 
-get('/admin/tags') do
-  erb(:'tags/index', locals: {
-        tags: database.all_of('tag'),
-        tag_purposes: database.all_of('tag_purpose')
-      })
-end
-
-get('/admin/users') do
-  erb(:'users/index', locals: {
-        users: database.all_of('user').drop(1)
-      })
+    base.get('/admin/users') do
+      erb(:'users/index', locals: {
+            users: database.all_of('user').drop(1)
+          })
+    end
+  end
 end
